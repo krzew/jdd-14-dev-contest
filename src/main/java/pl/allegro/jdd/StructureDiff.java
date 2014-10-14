0a20416413071f3a5945a95bc193de22374456d7
krzew <eol0@o2.pl>
2014-10-14 15:25:24 +0200

    Changed to using JaVers API, cleaned up the code

--- src/main/java/pl/allegro/jdd/StructureDiff.java
+++ src/main/java/pl/allegro/jdd/StructureDiff.java
@@ -2,17 +2,13 @@ package pl.allegro.jdd;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.javers.core.Javers;
 import org.javers.core.JaversBuilder;
-import org.javers.core.diff.Change;
 import org.javers.core.diff.Diff;
 import org.javers.core.diff.changetype.NewObject;
 import org.javers.core.diff.changetype.ObjectRemoved;
-import org.javers.core.diff.changetype.PropertyChange;
 
 import pl.allegro.jdd.Changes;
 import pl.allegro.jdd.Employee;
@@ -29,18 +25,11 @@ public class StructureDiff {
         
         Diff javersDiff = javers.compare(oldCTO, newCTO);
 
-        List<Employee> fired = getEmployeesFromChanges(javersDiff.getChanges((change)->(change instanceof ObjectRemoved)));
-        List<Employee> hired = getEmployeesFromChanges(javersDiff.getChanges((change)->(change instanceof NewObject)));
-        List<Employee> changed = getEmployeesFromChanges( javersDiff.getChanges((change)->(change instanceof PropertyChange && ((PropertyChange)change).getProperty().getName().equals(SALARY_PROPERTY_NAME))));
+        List<Employee> fired = javersDiff.getObjectsByChangeType(ObjectRemoved.class);
+        List<Employee> hired = javersDiff.getObjectsByChangeType(NewObject.class);
+        List<Employee> changed = javersDiff.getObjectsWithChangedProperty(SALARY_PROPERTY_NAME);
         
         return new Changes(fired,hired,changed);
-    }
-    
-    private List<Employee> getEmployeesFromChanges(List<Change> changesList){
-    	if(null == changesList || changesList.isEmpty()){
-    		return Collections.emptyList();
-    	}
-    	return changesList.stream().map(change->{return (Employee)(change.getAffectedCdo());}).collect(Collectors.toList());
     }
     
 	
